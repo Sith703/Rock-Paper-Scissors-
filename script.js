839df576c355e4925602943f3bb8da092e370d68
Sith703 <harry_joycedavis@hotmail.com>
2024-01-02 18:07:36 +0700

    Change the function to allow multiple rounds

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice () {
     let  randomNumber = Math.floor(Math.random() * 4)
     if (randomNumber == 1) {
-        return "Rock"
+        return "rock"
     }
     else if (randomNumber == 2) {
         return "paper"
@@ -13,3 +13,22 @@ function getComputerChoice () {
 }
 
 console.log(getComputerChoice())
+
+function playRound(_playerSelection, computerSelection) {
+    
+     if (computerSelection == "scissors") {
+    return "You Win! You're the best"
+}
+    else if (computerSelection == "paper") {
+    return "Haha you lose!"
+}
+    else (computerSelection == "rock") 
+    return "damn it do be drawin doe"
+
+
+}
+    
+let playerSelection = "rock" ;
+let computerSelection = getComputerChoice() ;
+
+console.log(playRound(playerSelection, computerSelection))
